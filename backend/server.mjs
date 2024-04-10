f1823ecdcc0de7c504ecb0c59ecbaca041838417
pinkflamingo108 <andoni.romano@aspentech.com>
2024-04-10 11:16:59 -0600

    react and tailwind

--- backend/server.mjs
+++ backend/server.mjs
@@ -23,6 +23,21 @@ const MainModel = mongoose.model(
       required: true,
       trim: true,
     },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
   })
 );
 
